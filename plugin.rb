8c97c818a6fabf4386867b6a6ecc93a0bbd588cb
sirius <sirius.tsou@gmail.com>
2019-07-24 20:26:44 +0800

    add new options

--- plugin.rb
+++ plugin.rb
@@ -1,13 +1,15 @@
 # name: discourse-eauth
 # about: Authenticate with Eauth
-# version: 0.1
+# version: 0.1.1
 # author: Sirius Tsou
 # url: https://github.com/pelith/discourse-eauth
 
 enabled_site_setting :eauth_auth_enabled
+enabled_site_setting :eauth_site
+enabled_site_setting :eauth_authorize_url
+enabled_site_setting :eauth_token_url
 enabled_site_setting :eauth_client_id
 enabled_site_setting :eauth_client_secret
-enabled_site_setting :eauth_site
 
 gem 'omniauth-eauth-oauth2', '1.0.1'
 
@@ -26,6 +28,8 @@ class Auth::EauthAuthenticator < Auth::ManagedAuthenticator
            setup: lambda { |env|
               strategy = env["omniauth.strategy"]
               strategy.options[:client_options][:site] = SiteSetting.eauth_site
+              strategy.options[:client_options][:authorize_url] = SiteSetting.eauth_authorize_url
+              strategy.options[:client_options][:token_url] = SiteSetting.eauth_token_url
               strategy.options[:client_id] = SiteSetting.eauth_client_id
               strategy.options[:client_secret] = SiteSetting.eauth_client_secret
            }
@@ -39,6 +43,7 @@ class Auth::EauthAuthenticator < Auth::ManagedAuthenticator
   end
 
   def after_authenticate(auth_token, existing_account: nil)
+    auth_token[:extra] = {}
     super
   end
 end
