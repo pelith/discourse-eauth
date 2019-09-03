c29ea0ca14c546fc5a908e4dbd198684eac06565
Gilg4mesh <freedomtomdestiny@gmail.com>
2019-09-03 16:02:36 +0800

    association

--- plugin.rb
+++ plugin.rb
@@ -36,17 +36,54 @@ class Auth::EauthAuthenticator < Auth::ManagedAuthenticator
   end
 
   def description_for_user(user)
-    info = UserAssociatedAccount.find_by(provider_name: name, user_id: user.id)&.info
-    return "" if info.nil?
+    associated_account = UserAssociatedAccount.find_by(provider_name: name, user_id: user.id)
+    return "" if associated_account.nil?
+    description_for_auth_hash(associated_account) || I18n.t("associated_accounts.connected")
+  end
 
-    info["address"] || ""
+  def description_for_auth_hash(auth_token)
+    return if auth_token&.info.nil?
+    info = auth_token.info
+    info["address"]
   end
 
   def after_authenticate(auth_token, existing_account: nil)
-    auth_token[:extra] = {}
-    super
+    # Try and find an association for this account
+    association = UserAssociatedAccount.find_or_initialize_by(provider_name: auth_token[:provider], provider_uid: auth_token[:uid])
+
+    # Reconnecting to existing account
+    if can_connect_existing_user? && existing_account && (association.user.nil? || existing_account.id != association.user_id)
+      association.user = existing_account
+    end
+
+    association.info = auth_token[:info] || {}
+    association.credentials = auth_token[:credentials] || {}
+    association.extra = auth_token[:extra] || {}
+
+    association.last_used = Time.zone.now
+
+    # Save to the DB. Do this even if we don't have a user - it might be linked up later in after_create_account
+    association.save!
+
+    # Build the Auth::Result object
+    result = Auth::Result.new
+    info = auth_token[:info]
+    result.omit_username = true
+
+    result.email = info[:address]+'@ethmail.cc'
+    result.name = ''
+
+    result.extra_data = {
+      provider: auth_token[:provider],
+      uid: auth_token[:uid]
+    }
+    result.user = association.user
+
+    result
   end
 end
+
+
 
 auth_provider frame_width: 920,
               frame_height: 800,
@@ -60,3 +97,4 @@ register_css <<CSS
   background: #c99d66;
 }
 CSS
+
